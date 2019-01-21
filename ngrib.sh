6c371cfd1feb47ca62d2b0a7c0a48ccdd1c720e0
Remo Tomasi <remo.tomasi@gmail.com>
2019-01-21 17:27:57 +0100

    Main program and list of grib labels
    In this first commit there two files:
    - ngrib.sh the main program wrote in bash shell language. The program uses, in turn, the wgrib application (freely downloadable).
    - lists.txt is the list of the grib parameters together their explanations.

---
+++ ngrib.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+#################################################################################
+#	BASH program to download and parse grib2 files and generate a final csv file
+#	Thanks to NOAA - http://nomads.ncep.noaa.gov
+#
+#	by Remo Tomasi
+#	21-01-2019 v0.07
+#
+#	- Reordered columns
+#################################################################################
+
+today=$(date +%Y%m%d)
+
+o=00
+o=$(date +%H)
+hh=""
+if [ "$o" -ge 2 ] && [ "$o" -lt 8 ]
+	then hh="18"
+		today=$(date +%Y%m%d -d "yesterday")
+elif [ "$o" -ge 8 ] && [ "$o" -lt 14 ]
+	then hh="00"
+elif [ "$o" -ge 14 ] && [ "$o" -lt 20 ]
+	then hh="06"
+elif [ "$o" -ge 20 ] || [ "$o" -lt 2 ]
+	then hh="12"
+fi
+
+#today="20190115"
+
+h=(000 003 006 009 012 015 018 021 024 027 030 033 036 039 042 045 048 051 054 057 060 063 066 069 072 075 078 081 084 087 090 093 096 099 102 105 108 111 114 117 120)
+
+if [ -e "final.csv" ]; then rm final.csv; fi								# remove final.csv
+for i in ${h[*]}; do $(if [ -e "pro$i" ]; then rm pro$i; fi); done			# remove pro#.csv
+for i in ${h[*]}; do $(if [ -e "csv$i.csv" ]; then rm csv$i.csv; fi); done	# remove csv#.csv
+#for i in {1..6}; do $(if [ -e "p$i.csv" ]; then rm p$i.csv; fi); done		# remove p#.csv
+
+for i in ${h[*]}; do curl "http://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_0p25.pl?file=gfs.t${hh}z.pgrb2.0p25.f${i}&lev_0C_isotherm=on&lev_1000_mb=on&lev_10_m_above_ground=on&lev_2_m_above_ground=on&lev_500_mb=on&lev_700_mb=on&lev_850_mb=on&lev_high_cloud_layer=on&lev_low_cloud_layer=on&lev_mean_sea_level=on&lev_middle_cloud_layer=on&lev_surface=on&lev_tropopause=on&var_ABSV=on&var_ACPCP=on&var_APCP=on&var_CAPE=on&var_CFRZR=on&var_CICEP=on&var_CIN=on&var_CPOFP=on&var_CPRAT=on&var_CRAIN=on&var_CSNOW=on&var_DPT=on&var_GUST=on&var_HGT=on&var_ICEC=on&var_LFTX=on&var_PEVPR=on&var_PRATE=on&var_PRES=on&var_PRMSL=on&var_RH=on&var_SNOD=on&var_SUNSD=on&var_TCDC=on&var_TMP=on&var_UGRD=on&var_VGRD=on&var_VIS=on&var_VVEL=on&var_VWSH=on&subregion=&leftlon=18&rightlon=18&toplat=40&bottomlat=40&dir=%2Fgfs.${today}${hh}" -o pro$i; done
+
+for i in ${h[*]}; do ./wgrib2.exe pro$i -csv csv$i.csv; done						# obtain infos from gribs to csvs
+for i in ${h[*]}; do cat csv$i.csv >> final.csv; done								# merge of csvs
+for i in ${h[*]}; do cut -d',' -f7 csv$i.csv | paste -s; done  | tail -n +2 > p.csv # union of the values of all the grib files
+for i in ${h[*]}; do cut -d',' -f2 csv$i.csv | head -1; done | tail -n +2> p1.csv	# obtain date column
+
+echo ',' > p2.csv							# adding a comma in p2.csv temporary file
+cut -d',' -f3 csv033.csv >> p2.csv			# adding labels + sublabels to p2.csv: labels complete!
+echo ',' > p2b.csv							# adding a comma in p2b.csv temporary file
+cut -d',' -f4 csv033.csv >> p2b.csv			# atmosphere labels
+cat p2.csv | paste -s > p2c.csv				# labels
+cat p2b.csv | paste -s >> p2c.csv			# labels + atmosphere labels
+cat p2c.csv | tr ' ' '_' > p2d.csv			# transform spaces to _ (to obtain one word")
+paste -d',' p1.csv p.csv > p5.csv			# adding date to datas
+
+awk 'BEGIN{FS=OFS=";"}{if (NR>=3&&NR<=42) print $1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19,$20,$13,$14,$15,$16,$17,$18,$19,$20,$21,$22,$23,$24,$25,$26,$27,$28,$29,$30,$31,$32,$33,$34,$35,$36,$37,$38,$39,$40,$41,$42 }' p5.csv
+cut -d',' -f2 p2d.csv > p3.csv						# taking the 2nd column after the comma
+cat p5.csv >> p3.csv								# adding labels + (date + datas)
+cat p3.csv | tr '\t' ';' | tr ',' ';' > p6.csv		# convert tabs and commas into semicolon
+
+cat p6.csv | cut -d';' -f6,7,8,9,13,14,15,16,20,21,22,23,25,26,27,28,29,32,33,34,58,60,61,62,65 --complement > p6d.csv
+
+awk 'BEGIN{FS=OFS=";"}{if (NR>=1&&NR<=2) print $1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19,$20,$21,$22,$23,$24,$25,$26,$27,$28,$29,$30,$31,$32,$33,$34,$35,$36,$37,$38,$39,$40,$41,$42 }' p6d.csv > p6e.csv
+
+awk 'BEGIN{FS=OFS=";"}{if (NR>=3&&NR<=42) print $1,$2,$3,$4,$5-273.15,$6,$7,$8-273.15,$9,$10,$11-273.15,$12,$13,$14,$15,$16,$17-273.15,$18-273.15,$19,$20,$21,$22,$23,$24,$25,$26,$27,$28,$29,$30,$31/3600,$32,$33,$34,$35,$36,$37,$38,$39,$40,$41,$42/100 }' p6d.csv > p6f.csv
+
+cat p6f.csv >> p6e.csv
+
+awk 'BEGIN{FS=OFS=";"}{ print $1,$17,$18,$19,$20,$21,$3,$35,$36,$37,$42,$23,$24,$25,$26,$30,$29,$28,$22,$27,$15,$32,$33,$38,$4,$7,$10,$14,$41,$5,$8,$11,$6,$9,$12,$13,$2,$16,$39,$31 }' p6e.csv > final.csv
