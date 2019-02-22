aadb3256ad66c2dac53dae7f82b42c9f2312e57d
remotomasi <remo.tomasi@gmail.com>
2019-02-22 23:07:05 +0100

    Possibility to know the OS on which operate

--- ngrib.sh
+++ ngrib.sh
@@ -4,7 +4,7 @@
 #	Thanks to NOAA - http://nomads.ncep.noaa.gov
 #
 #	by Remo Tomasi
-#	12-02-2019 v0.21
+#	22-02-2019 v0.3
 #
 #################################################################################
 
@@ -31,14 +31,32 @@ fi
 
 h=(000 003 006 009 012 015 018 021 024 027 030 033 036 039 042 045 048 051 054 057 060 063 066 069 072 075 078 081 084 087 090 093 096 099 102 105 108 111 114 117 120)
 
+# OS VERSION
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+#echo ${machine}
+if [ $machine = "Linux" ]
+	then
+		command="wgrib2"
+	else
+		command="wgrib2.exe"
+fi
+
 # curl VERSION
 # for i in ${h[*]}; do curl "http://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_0p25.pl?file=gfs.t${hh}z.pgrb2.0p25.f${i}&lev_0C_isotherm=on&lev_1000_mb=on&lev_10_m_above_ground=on&lev_2_m_above_ground=on&lev_500_mb=on&lev_700_mb=on&lev_850_mb=on&lev_high_cloud_layer=on&lev_low_cloud_layer=on&lev_mean_sea_level=on&lev_middle_cloud_layer=on&lev_surface=on&lev_tropopause=on&var_ABSV=on&var_ACPCP=on&var_APCP=on&var_CAPE=on&var_CFRZR=on&var_CICEP=on&var_CIN=on&var_CPOFP=on&var_CPRAT=on&var_CRAIN=on&var_CSNOW=on&var_DPT=on&var_GUST=on&var_HGT=on&var_ICEC=on&var_LFTX=on&var_PEVPR=on&var_PRATE=on&var_PRES=on&var_PRMSL=on&var_RH=on&var_SNOD=on&var_SUNSD=on&var_TCDC=on&var_TMP=on&var_UGRD=on&var_VGRD=on&var_VIS=on&var_VVEL=on&var_VWSH=on&subregion=&leftlon=$lon&rightlon=$lon&toplat=$lat&bottomlat=$lat&dir=%2Fgfs.${today}${hh}" -o pro$i; done
+
 # wget VERSION
 for i in ${h[*]};
 	do
 		#wget "http://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_0p25.pl?file=gfs.t${hh}z.pgrb2.0p25.f${i}&lev_0C_isotherm=on&lev_1000_mb=on&lev_10_m_above_ground=on&lev_2_m_above_ground=on&lev_500_mb=on&lev_700_mb=on&lev_850_mb=on&lev_high_cloud_layer=on&lev_low_cloud_layer=on&lev_mean_sea_level=on&lev_middle_cloud_layer=on&lev_surface=on&lev_tropopause=on&var_ABSV=on&var_ACPCP=on&var_APCP=on&var_CAPE=on&var_CFRZR=on&var_CICEP=on&var_CIN=on&var_CPOFP=on&var_CPRAT=on&var_CRAIN=on&var_CSNOW=on&var_DPT=on&var_GUST=on&var_HGT=on&var_ICEC=on&var_LFTX=on&var_PEVPR=on&var_PRATE=on&var_PRES=on&var_PRMSL=on&var_RH=on&var_SNOD=on&var_SUNSD=on&var_TCDC=on&var_TMP=on&var_UGRD=on&var_VGRD=on&var_VIS=on&var_VVEL=on&var_VWSH=on&subregion=&leftlon=$lon&rightlon=$lon&toplat=$lat&bottomlat=$lat&dir=%2Fgfs.${today}${hh}"  2>/dev/null -O - > pro$i
 		wget "http://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_0p25.pl?file=gfs.t${hh}z.pgrb2.0p25.f${i}&lev_0C_isotherm=on&lev_1000_mb=on&lev_10_m_above_ground=on&lev_2_m_above_ground=on&lev_500_mb=on&lev_700_mb=on&lev_850_mb=on&lev_high_cloud_layer=on&lev_low_cloud_layer=on&lev_mean_sea_level=on&lev_middle_cloud_layer=on&lev_surface=on&lev_tropopause=on&var_ABSV=on&var_ACPCP=on&var_APCP=on&var_CAPE=on&var_CFRZR=on&var_CICEP=on&var_CIN=on&var_CPOFP=on&var_CPRAT=on&var_CRAIN=on&var_CSNOW=on&var_DPT=on&var_GUST=on&var_HGT=on&var_ICEC=on&var_LFTX=on&var_PEVPR=on&var_PRATE=on&var_PRES=on&var_PRMSL=on&var_RH=on&var_SNOD=on&var_SUNSD=on&var_TCDC=on&var_TMP=on&var_UGRD=on&var_VGRD=on&var_VIS=on&var_VVEL=on&var_VWSH=on&subregion=&leftlon=$lon&rightlon=$lon&toplat=$lat&bottomlat=$lat&dir=%2Fgfs.${today}${hh}"  2>/dev/null -O - > pro$i
-		./wgrib2.exe pro$i -csv csv$i.csv			# obtain infos from gribs to csvs
+		./$command pro$i -csv csv$i.csv			# obtain infos from gribs to csvs
 		cat csv$i.csv >> final.csv						# merge of csvs
 done
 for i in ${h[*]}; do cut -d',' -f7 csv$i.csv | paste -s; done  | tail -n +2 > p.csv # union of the values of all the grib files
@@ -54,9 +72,9 @@ cat p2c.csv | tr ' ' '_' > p2d.csv			# transform spaces to _ (to obtain one word
 paste -d',' p1.csv p.csv > p5.csv				# adding date to datas
 
 awk 'BEGIN{FS=OFS=";"}{if (NR>=3&&NR<=42) print $1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19,$20,$13,$14,$15,$16,$17,$18,$19,$20,$21,$22,$23*3600,$24*3600,$25,$26,$27,$28,$29,$30,$31,$32,$33,$34,$35,$36,$37,$38,$39,$40,$41,$42 }' p5.csv
-cut -d',' -f2 p2d.csv > p3.csv			# taking the 2nd $1umn after the comma
-cat p5.csv >> p3.csv								# adding labels + (date + datas)
-cat p3.csv | tr '\t' ',' > p6.csv		# convert tabs and commas into semi$1on
+cut -d',' -f2 p2d.csv > p3.csv					# taking the 2nd $1umn after the comma
+cat p5.csv >> p3.csv										# adding labels + (date + datas)
+cat p3.csv | tr '\t' ',' > p6.csv				# convert tabs and commas into semi$1on
 
 cat p6.csv | cut -d',' -f6,7,8,9,13,14,15,16,20,21,22,23,25,26,27,28,29,32,33,34,58,60,61,62,65 --complement > p6d.csv
 
@@ -92,8 +110,7 @@ function windDirection(x) {
 }
 {if (NR>=3&&NR<=42) print windDirection((atan2($20,$21)*57.3+180)) }' p6d.csv > p6f.csv			# added function that transform direction from degree to a capital letter
 
-sed -i '/^$/d' p6f.csv				# delete void lines
-# "'"`windDirection "$44"`"'"
+sed -i '/^$/d' p6f.csv																			# delete void lines
 cat p6f.csv >> p6e.csv
 
 sed -i -e 's/2_m_above_ground/2m/g;s/10_m_above_ground/10m/g;s/low_cloud_layer/low_cloud/g' p6e.csv
@@ -111,14 +128,14 @@ echo -e "<html><body><table style='font-family:"Arial", Courier, monospace; font
 echo -e "</font></table></body></html>" >> final.html
 
 # cleaning of some files
-if [ -e "final.csv" ]; then rm final.csv; fi																# remove final.csv
+if [ -e "final.csv" ]; then rm final.csv; fi								# remove final.csv
 for i in ${h[*]};
 	do
 		$(if [ -e "pro$i" ]; then rm pro$i; fi)
 		$(if [ -e "csv$i.csv" ]; then rm csv$i.csv; fi)
-done	# remove pro*.csv and csv*.csv
-rm p*.csv																																		# remove all p*.csv
+done
 
+rm p*.csv																										# remove all p*.csv
 
 # graphic part
 cat final.txt | tr '|' ' ' | tr ',' ' ' | tr '"' ' ' | cut -d' ' -f2- > final2.csv
@@ -137,5 +154,4 @@ gnuplot ./precTypes.pg > precTypes.png
 convert \( weather.png precipitations.png -append \) \( clouds.png pressureWind.png -append \) \( hgt.png temperatures.png -append \) \( cape-lftx.png precTypes.png -append \) +append weatherForecastFinal.png
 
 # clean last created files
-
-rm fi
+rm final2.csv final3.csv
