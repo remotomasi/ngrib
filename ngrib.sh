3f9cd9891fa123ea53eb25468d8e76e4d4c6a9d4
remotomasi <remo.tomasi@gmail.com>
2019-04-16 17:30:12 +0200

    Improved various graphs

--- ngrib.sh
+++ ngrib.sh
@@ -5,11 +5,14 @@
 #
 #	by Remo Tomasi
 #	24-02-2019 v0.31
-#
+#	16-04-2019 v0.5
 #################################################################################
 
-echo -e "Insert latitude and longitude separated by spaces (i.e.: 60.75 21.34)"
-read lat lon
+#echo -e "Insert latitude and longitude separated by spaces (i.e.: 60.75 21.34)"
+#read lat lon
+
+lat=$1
+lon=$2
 
 # calculation of the coordinates of the square area
 lat1=`echo $lat+0.1|bc`
@@ -67,25 +70,25 @@ for i in ${h[*]};
 	do
 		#wget "http://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_0p25.pl?file=gfs.t${hh}z.pgrb2.0p25.f${i}&lev_0C_isotherm=on&lev_1000_mb=on&lev_10_m_above_ground=on&lev_2_m_above_ground=on&lev_500_mb=on&lev_700_mb=on&lev_850_mb=on&lev_high_cloud_layer=on&lev_low_cloud_layer=on&lev_mean_sea_level=on&lev_middle_cloud_layer=on&lev_surface=on&lev_tropopause=on&var_ABSV=on&var_ACPCP=on&var_APCP=on&var_CAPE=on&var_CFRZR=on&var_CICEP=on&var_CIN=on&var_CPOFP=on&var_CPRAT=on&var_CRAIN=on&var_CSNOW=on&var_DPT=on&var_GUST=on&var_HGT=on&var_ICEC=on&var_LFTX=on&var_PEVPR=on&var_PRATE=on&var_PRES=on&var_PRMSL=on&var_RH=on&var_SNOD=on&var_SUNSD=on&var_TCDC=on&var_TMP=on&var_UGRD=on&var_VGRD=on&var_VIS=on&var_VVEL=on&var_VWSH=on&subregion=&leftlon=$lon&rightlon=$lon&toplat=$lat&bottomlat=$lat&dir=%2Fgfs.${today}${hh}"  2>/dev/null -O - > pro$i
 		wget "http://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_0p25.pl?file=gfs.t${hh}z.pgrb2.0p25.f${i}&lev_0C_isotherm=on&lev_1000_mb=on&lev_10_m_above_ground=on&lev_2_m_above_ground=on&lev_500_mb=on&lev_700_mb=on&lev_850_mb=on&lev_high_cloud_layer=on&lev_low_cloud_layer=on&lev_mean_sea_level=on&lev_middle_cloud_layer=on&lev_surface=on&lev_tropopause=on&var_ABSV=on&var_ACPCP=on&var_APCP=on&var_CAPE=on&var_CFRZR=on&var_CICEP=on&var_CIN=on&var_CPOFP=on&var_CPRAT=on&var_CRAIN=on&var_CSNOW=on&var_DPT=on&var_GUST=on&var_HGT=on&var_ICEC=on&var_LFTX=on&var_PEVPR=on&var_PRATE=on&var_PRES=on&var_PRMSL=on&var_RH=on&var_SNOD=on&var_SUNSD=on&var_TCDC=on&var_TMP=on&var_UGRD=on&var_VGRD=on&var_VIS=on&var_VVEL=on&var_VWSH=on&subregion=&leftlon=$lon2&rightlon=$lon1&toplat=$lat1&bottomlat=$lat2&dir=%2Fgfs.${today}${hh}"  2>/dev/null -O - > pro$i
-		./$command pro$i -csv csv$i.csv			# obtain infos from gribs to csvs
-		cat csv$i.csv >> final.csv					# merge of csvs
+		./$command pro$i -csv csv$i.csv		# obtain infos from gribs to csvs
+		cat csv$i.csv >> final.csv			# merge of csvs
 done
-for i in ${h[*]}; do cut -d',' -f7 csv$i.csv | paste -s; done  | tail -n +2 > p.csv # union of the values of all the grib files
-for i in ${h[*]}; do cut -d',' -f2 csv$i.csv | head -1; done | tail -n +2> p1.csv		# obtain date $1umn
+for i in ${h[*]}; do cut -d',' -f7 csv$i.csv | paste -s; done  | tail -n +2 > p.csv 	# union of the values of all the grib files
+for i in ${h[*]}; do cut -d',' -f2 csv$i.csv | head -1; done | tail -n +2 > p1.csv		# obtain date $1umn
 
-echo ',' > p2.csv												# adding a comma in p2.csv temporary file
+echo ',' > p2.csv							# adding a comma in p2.csv temporary file
 cut -d',' -f3 csv033.csv >> p2.csv			# adding labels + sublabels to p2.csv: labels complete!
-echo ',' > p2b.csv											# adding a comma in p2b.csv temporary file
+echo ',' > p2b.csv							# adding a comma in p2b.csv temporary file
 cut -d',' -f4 csv033.csv >> p2b.csv			# atmosphere labels
-cat p2.csv | paste -s > p2c.csv					# labels
-cat p2b.csv | paste -s >> p2c.csv				# labels + atmosphere labels
+cat p2.csv | paste -s > p2c.csv				# labels
+cat p2b.csv | paste -s >> p2c.csv			# labels + atmosphere labels
 cat p2c.csv | tr ' ' '_' > p2d.csv			# transform spaces to _ (to obtain one word")
-paste -d',' p1.csv p.csv > p5.csv				# adding date to datas
+paste -d',' p1.csv p.csv > p5.csv			# adding date to datas
 
 awk 'BEGIN{FS=OFS=";"}{if (NR>=3&&NR<=46) print $1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19,$20,$13,$14,$15*100,$16,$17,$18,$19,$20,$21,$22,$23*3600,$24*3600,$25,$26,$27,$28,$29,$30,$31,$32,$33,$34,$35,$36,$37,$38,$39,$40,$41,$42 }' p5.csv
-cut -d',' -f2 p2d.csv > p3.csv					# taking the 2nd $1umn after the comma
-cat p5.csv >> p3.csv										# adding labels + (date + datas)
-cat p3.csv | tr '\t' ',' > p6.csv				# convert tabs and commas into semi$1on
+cut -d',' -f2 p2d.csv > p3.csv				# taking the 2nd $1umn after the comma
+cat p5.csv >> p3.csv						# adding labels + (date + datas)
+cat p3.csv | tr '\t' ',' > p6.csv			# convert tabs and commas into semi$1on
 
 cat p6.csv | cut -d',' -f6,7,8,9,13,14,15,16,20,21,22,23,25,26,27,28,29,32,33,34,58,60,61,62,65 --complement > p6d.csv
 
@@ -121,7 +124,7 @@ function windDirection(x) {
 }
 {if (NR>=3&&NR<=46) print windDirection((atan2($20,$21)*57.3+180)) }' p6d.csv > p6f.csv			# added function that transform direction from degree to a capital letter
 
-sed -i '/^$/d' p6f.csv																			# delete void lines
+sed -i '/^$/d' p6f.csv						# delete void lines
 cat p6f.csv >> p6e.csv
 
 sed -i -e 's/2_m_above_ground/2m/g;s/10_m_above_ground/10m/g;s/low_cloud_layer/low_cloud/g' p6e.csv
@@ -144,7 +147,7 @@ sed -i -e 's/TCDC <\/td><td>100<\/td><td> PRMSL/TCDC <\/td><td> TCDC <\/td><td> 
 sed -i -e 's/middle_cloud <\/td><td>100<\/td><td> mean_sea/middle_cloud <\/td><td> high_cloud <\/td><td> mean_sea/g' final.html
 
 # cleaning of some files
-if [ -e "final.csv" ]; then rm final.csv; fi								# remove final.csv
+if [ -e "final.csv" ]; then rm final.csv; fi	# remove final.csv
 for i in ${h[*]};
 	do
 		$(if [ -e "pro$i" ]; then rm pro$i; fi)
