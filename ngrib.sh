805c61e5c9736aa151d892d3390c25e1776d71e0
remotomasi <remo.tomasi@gmail.com>
2019-06-19 01:37:20 +0200

    Adapted code to the new GFS

--- ngrib.sh
+++ ngrib.sh
@@ -63,13 +63,13 @@ fi
 
 # wget VERSION
 
-wget "http://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_0p25_1hr.pl?file=gfs.t${hh}z.pgrb2.0p25.anl&lev_0C_isotherm=on&lev_1000_mb=on&lev_10_m_above_ground=on&lev_2_m_above_ground=on&lev_500_mb=on&lev_700_mb=on&lev_850_mb=on&lev_high_cloud_layer=on&lev_low_cloud_layer=on&lev_mean_sea_level=on&lev_middle_cloud_layer=on&lev_surface=on&lev_tropopause=on&var_ABSV=on&var_ACPCP=on&var_APCP=on&var_CAPE=on&var_CFRZR=on&var_CICEP=on&var_CIN=on&var_CPOFP=on&var_CPRAT=on&var_CRAIN=on&var_CSNOW=on&var_DPT=on&var_GUST=on&var_HGT=on&var_ICEC=on&var_LFTX=on&var_PEVPR=on&var_PRATE=on&var_PRES=on&var_PRMSL=on&var_RH=on&var_SNOD=on&var_SUNSD=on&var_TCDC=on&var_TMP=on&var_UGRD=on&var_VGRD=on&var_VIS=on&var_VVEL=on&var_VWSH=on&subregion=&leftlon=$lon2&rightlon=$lon1&toplat=$lat1&bottomlat=$lat2&dir=%2Fgfs.${today}${hh}"  2>/dev/null -O - > anlFile
+wget "https://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_0p25_1hr.pl?file=gfs.t${hh}z.pgrb2.0p25.anl&lev_0C_isotherm=on&lev_1000_mb=on&lev_10_m_above_ground=on&lev_2_m_above_ground=on&lev_500_mb=on&lev_700_mb=on&lev_850_mb=on&lev_high_cloud_layer=on&lev_low_cloud_layer=on&lev_mean_sea_level=on&lev_middle_cloud_layer=on&lev_surface=on&lev_tropopause=on&var_ABSV=on&var_ACPCP=on&var_APCP=on&var_CAPE=on&var_CFRZR=on&var_CICEP=on&var_CIN=on&var_CPOFP=on&var_CPRAT=on&var_CRAIN=on&var_CSNOW=on&var_DPT=on&var_GUST=on&var_HGT=on&var_ICEC=on&var_LFTX=on&var_PEVPR=on&var_PRATE=on&var_PRES=on&var_PRMSL=on&var_RH=on&var_SNOD=on&var_SUNSD=on&var_TCDC=on&var_TMP=on&var_UGRD=on&var_VGRD=on&var_VIS=on&var_VVEL=on&var_VWSH=on&subregion=&leftlon=$lon2&rightlon=$lon1&toplat=$lat1&bottomlat=$lat2&dir=%2Fgfs.${today}%2F${hh}"  2>/dev/null -O - > anlFile
 run="$(./$command anlFile | head -n 1  | cut -d'=' -f2 | cut -c9-10)"
 
 for i in ${h[*]};
 	do
 		#wget "http://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_0p25.pl?file=gfs.t${hh}z.pgrb2.0p25.f${i}&lev_0C_isotherm=on&lev_1000_mb=on&lev_10_m_above_ground=on&lev_2_m_above_ground=on&lev_500_mb=on&lev_700_mb=on&lev_850_mb=on&lev_high_cloud_layer=on&lev_low_cloud_layer=on&lev_mean_sea_level=on&lev_middle_cloud_layer=on&lev_surface=on&lev_tropopause=on&var_ABSV=on&var_ACPCP=on&var_APCP=on&var_CAPE=on&var_CFRZR=on&var_CICEP=on&var_CIN=on&var_CPOFP=on&var_CPRAT=on&var_CRAIN=on&var_CSNOW=on&var_DPT=on&var_GUST=on&var_HGT=on&var_ICEC=on&var_LFTX=on&var_PEVPR=on&var_PRATE=on&var_PRES=on&var_PRMSL=on&var_RH=on&var_SNOD=on&var_SUNSD=on&var_TCDC=on&var_TMP=on&var_UGRD=on&var_VGRD=on&var_VIS=on&var_VVEL=on&var_VWSH=on&subregion=&leftlon=$lon&rightlon=$lon&toplat=$lat&bottomlat=$lat&dir=%2Fgfs.${today}${hh}"  2>/dev/null -O - > pro$i
-		wget "http://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_0p25.pl?file=gfs.t${hh}z.pgrb2.0p25.f${i}&lev_0C_isotherm=on&lev_1000_mb=on&lev_10_m_above_ground=on&lev_2_m_above_ground=on&lev_500_mb=on&lev_700_mb=on&lev_850_mb=on&lev_high_cloud_layer=on&lev_low_cloud_layer=on&lev_mean_sea_level=on&lev_middle_cloud_layer=on&lev_surface=on&lev_tropopause=on&var_ABSV=on&var_ACPCP=on&var_APCP=on&var_CAPE=on&var_CFRZR=on&var_CICEP=on&var_CIN=on&var_CPOFP=on&var_CPRAT=on&var_CRAIN=on&var_CSNOW=on&var_DPT=on&var_GUST=on&var_HGT=on&var_ICEC=on&var_LFTX=on&var_PEVPR=on&var_PRATE=on&var_PRES=on&var_PRMSL=on&var_RH=on&var_SNOD=on&var_SUNSD=on&var_TCDC=on&var_TMP=on&var_UGRD=on&var_VGRD=on&var_VIS=on&var_VVEL=on&var_VWSH=on&subregion=&leftlon=$lon2&rightlon=$lon1&toplat=$lat1&bottomlat=$lat2&dir=%2Fgfs.${today}${hh}"  2>/dev/null -O - > pro$i
+		wget "https://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_0p25.pl?file=gfs.t${hh}z.pgrb2.0p25.f${i}&lev_0C_isotherm=on&lev_1000_mb=on&lev_10_m_above_ground=on&lev_2_m_above_ground=on&lev_500_mb=on&lev_700_mb=on&lev_850_mb=on&lev_high_cloud_layer=on&lev_low_cloud_layer=on&lev_mean_sea_level=on&lev_middle_cloud_layer=on&lev_surface=on&lev_tropopause=on&var_ABSV=on&var_ACPCP=on&var_APCP=on&var_CAPE=on&var_CFRZR=on&var_CICEP=on&var_CIN=on&var_CPOFP=on&var_CPRAT=on&var_CRAIN=on&var_CSNOW=on&var_DPT=on&var_GUST=on&var_HGT=on&var_ICEC=on&var_LFTX=on&var_PEVPR=on&var_PRATE=on&var_PRES=on&var_PRMSL=on&var_RH=on&var_SNOD=on&var_SUNSD=on&var_TCDC=on&var_TMP=on&var_UGRD=on&var_VGRD=on&var_VIS=on&var_VVEL=on&var_VWSH=on&subregion=&leftlon=$lon2&rightlon=$lon1&toplat=$lat1&bottomlat=$lat2&dir=%2Fgfs.${today}%2F${hh}"  2>/dev/null -O - > pro$i
 		./$command pro$i -csv csv$i.csv		# obtain infos from gribs to csvs
 		cat csv$i.csv >> final.csv			# merge of csvs
 done
@@ -77,52 +77,53 @@ for i in ${h[*]}; do cut -d',' -f7 csv$i.csv | paste -s; done  | tail -n +2 > p.
 for i in ${h[*]}; do cut -d',' -f2 csv$i.csv | head -1; done | tail -n +2 > p1.csv		# obtain date $1umn
 
 echo ',' > p2.csv							# adding a comma in p2.csv temporary file
-cut -d',' -f3 csv033.csv >> p2.csv			# adding labels + sublabels to p2.csv: labels complete!
+cut -d',' -f3 csv003.csv >> p2.csv			# adding labels + sublabels to p2.csv: labels complete!
 echo ',' > p2b.csv							# adding a comma in p2b.csv temporary file
-cut -d',' -f4 csv033.csv >> p2b.csv			# atmosphere labels
+cut -d',' -f4 csv003.csv >> p2b.csv			# atmosphere labels
 cat p2.csv | paste -s > p2c.csv				# labels
 cat p2b.csv | paste -s >> p2c.csv			# labels + atmosphere labels
 cat p2c.csv | tr ' ' '_' > p2d.csv			# transform spaces to _ (to obtain one word")
 paste -d',' p1.csv p.csv > p5.csv			# adding date to datas
 
-awk 'BEGIN{FS=OFS=";"}{if (NR>=3&&NR<=46) print $1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19,$20,$13,$14,$15*100,$16,$17,$18,$19,$20,$21,$22,$23,$24,$25,$26,$27,$28,$29,$30,$31,$32,$33,$34,$35,$36,$37,$38,$39,$40,$41,$42 }' p5.csv
-cut -d',' -f2 p2d.csv > p3.csv				# taking the 2nd $1umn after the comma
+awk 'BEGIN{FS=OFS=";"}{if (NR>=3&&NR<=46) print $1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19,$20,$21,$22,$23,$24,$25,$26,$27,$28,$29,$30,$31,$32,$33,$34,$35,$36,$37,$38,$39,$40,$41,$42,$43,$44,$45,$46,$47,$48,$49,$50,$51,$52,$53,$54,$55,$56,$57,$58,$59,$60,$61,$62,$63,$64,$65,$66,$66,$67,$68,$69,$70,$71,$72,$73,$74,$75,$76,$77,$78,$79 }' p5.csv
+cut -d',' -f2 p2d.csv > p3.csv				# taking the 2nd column after the comma
 cat p5.csv >> p3.csv						# adding labels + (date + datas)
-cat p3.csv | tr '\t' ',' > p6.csv			# convert tabs and commas into semi$1on
+cat p3.csv | tr '\t' ',' > p6.csv			# convert tabs and commas into semicolon
 
-cat p6.csv | cut -d',' -f6,7,8,9,13,14,15,16,20,21,22,23,25,26,27,28,29,32,33,34,58,60,61,62,65 --complement > p6d.csv
+#cat p6.csv | cut -d',' -f6,7,8,9,13,14,15,16,20,21,22,23,25,26,27,28,29,32,33,34,58,60,61,62,65 --complement > p6d.csv
+#cat p6.csv | cut -d',' -f6,7,8,9,10,13,14,15,16,17,18,19,22,23,24,25,26,27,28,29,30,31,32,33,34,70,71,72,73,74,75,77 --complement > p6d.csv
+cat p6.csv > p6d.csv
 
-awk 'BEGIN{FS=OFS=","}{if (NR>=1&&NR<=2) print $1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19,$20,$21,$22,$23,$24,$25,$26,$27,$28,$29,$30,$31,$32,$33,$34,$35,$36,$37,$38,$39,$40,$41,$42 }' p6d.csv > p6e.csv
+awk 'BEGIN{FS=OFS=","}{if (NR>=1&&NR<=2) print $1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19,$20,$21,$22,$23,$24,$25,$26,$27,$28,$29,$30,$31,$32,$33,$34,$35,$36,$37,$38,$39,$40,$41,$42,$43,$44,$45,$46,$47,$48,$49,$50,$51,$52,$53,$54,$55,$56,$57,$58,$59,$60,$61,$62,$63,$64,$65,$66,$66,$67,$68,$69,$70,$71,$72,$73,$74,$75,$76,$77,$78,$79 }' p6d.csv > p6e.csv
 
-col="$1,$2,$3*3.6,$4,$5-273.15,$6,$7,$8-273.15,$9,$10,$11-273.15,$12,$13,$14,$15,$16,$17-273.15,$18-273.15,$19,$20*3.6,$21*3.6,$22,$23,$24,$25,$26,$27,$28,$29,$30,$31/3600,$32,$33,$34,$35,$36,$37,$38,$39,$40,$41,$42/100,(sqrt($20*$20+$21*$21))*3.6,(atan2($20,$21)*57.3+180)"
 awk 'BEGIN{FS=OFS=","}
 function windDirection(x) {
 	if ((x > 335) || (x <= 25)) {
-		print $1,$2,$3*3.6,$4,$5-273.15,$6,$7,$8-273.15,$9,$10,$11-273.15,$12,$13,$14,$15,$16,$17-273.15,$18-273.15,$19,$20*3.6,$21*3.6,$22,$23*3600,$24*3600,$25,$26,$27,$28,$29,$30,$31/3600,$32,$33,$34,$35,$36,$37,$38,$39,$40,$41,$42/100,(sqrt($20*$20+$21*$21))*3.6,(atan2($20,$21)*57.3+180),"N"                   #North (Tramontana)
+		print $1,$2,$3*3.6,$4,$5-273.15,$6,$7,$8,$9*3.6,$10*3.6,$11,$12,$13-273.15,$14,$15,$16,$17*3.6,$18*3.6,$19,$20,$21-273.15,$22,$23,$24,$25*3.6,$26*3.6,$27,$28-273.15,$29,$30,$31,$32*3.6,$33*3.6,$34,$35,$36,$37,$38-273.15,$39,$40,$41-273.15,$42,$43,$44*3.6,$45*3.6,$46,$47,$48,$49,$50,$51,$52,$53,$54,$55,$56,$57,$58,$59,$60,$61,$62,$63,$64,$65,$66,$67,$68,$69,$70/100,$71,$72-273.15,$73*3.6,$74*3.6,$75,$76,$77,$78,$79/100,(sqrt($44*$44+$45*$45))*3.6,(atan2($44,$45)*57.3+180),"N"                   #North (Tramontana)
 	}
 	else if	((x > 25) && (x <= 65)) {
-		print $1,$2,$3*3.6,$4,$5-273.15,$6,$7,$8-273.15,$9,$10,$11-273.15,$12,$13,$14,$15,$16,$17-273.15,$18-273.15,$19,$20*3.6,$21*3.6,$22,$23*3600,$24*3600,$25,$26,$27,$28,$29,$30,$31/3600,$32,$33,$34,$35,$36,$37,$38,$39,$40,$41,$42/100,(sqrt($20*$20+$21*$21))*3.6,(atan2($20,$21)*57.3+180),"NE"                  #North-East (Grecale)
+		print $1,$2,$3*3.6,$4,$5-273.15,$6,$7,$8,$9*3.6,$10*3.6,$11,$12,$13-273.15,$14,$15,$16,$17*3.6,$18*3.6,$19,$20,$21-273.15,$22,$23,$24,$25*3.6,$26*3.6,$27,$28-273.15,$29,$30,$31,$32*3.6,$33*3.6,$34,$35,$36,$37,$38-273.15,$39,$40,$41-273.15,$42,$43,$44*3.6,$45*3.6,$46,$47,$48,$49,$50,$51,$52,$53,$54,$55,$56,$57,$58,$59,$60,$61,$62,$63,$64,$65,$66,$67,$68,$69,$70/100,$71,$72-273.15,$73*3.6,$74*3.6,$75,$76,$77,$78,$79/100,(sqrt($44*$44+$45*$45))*3.6,(atan2($44,$45)*57.3+180),"NE"                   #North (Tramontana)
 	}
 	else if ((x > 65) && (x <= 115)) {
-		print $1,$2,$3*3.6,$4,$5-273.15,$6,$7,$8-273.15,$9,$10,$11-273.15,$12,$13,$14,$15,$16,$17-273.15,$18-273.15,$19,$20*3.6,$21*3.6,$22,$23*3600,$24*3600,$25,$26,$27,$28,$29,$30,$31/3600,$32,$33,$34,$35,$36,$37,$38,$39,$40,$41,$42/100,(sqrt($20*$20+$21*$21))*3.6,(atan2($20,$21)*57.3+180),"E"                   #East (Levante)
+		print $1,$2,$3*3.6,$4,$5-273.15,$6,$7,$8,$9*3.6,$10*3.6,$11,$12,$13-273.15,$14,$15,$16,$17*3.6,$18*3.6,$19,$20,$21-273.15,$22,$23,$24,$25*3.6,$26*3.6,$27,$28-273.15,$29,$30,$31,$32*3.6,$33*3.6,$34,$35,$36,$37,$38-273.15,$39,$40,$41-273.15,$42,$43,$44*3.6,$45*3.6,$46,$47,$48,$49,$50,$51,$52,$53,$54,$55,$56,$57,$58,$59,$60,$61,$62,$63,$64,$65,$66,$67,$68,$69,$70/100,$71,$72-273.15,$73*3.6,$74*3.6,$75,$76,$77,$78,$79/100,(sqrt($44*$44+$45*$45))*3.6,(atan2($44,$45)*57.3+180),"E"                   #North (Tramontana)
 	}
 	else if ((x > 115) && (x <= 155)) {
-		print $1,$2,$3*3.6,$4,$5-273.15,$6,$7,$8-273.15,$9,$10,$11-273.15,$12,$13,$14,$15,$16,$17-273.15,$18-273.15,$19,$20*3.6,$21*3.6,$22,$23*3600,$24*3600,$25,$26,$27,$28,$29,$30,$31/3600,$32,$33,$34,$35,$36,$37,$38,$39,$40,$41,$42/100,(sqrt($20*$20+$21*$21))*3.6,(atan2($20,$21)*57.3+180),"SE"                  #South-East (Scirocco)
+		print $1,$2,$3*3.6,$4,$5-273.15,$6,$7,$8,$9*3.6,$10*3.6,$11,$12,$13-273.15,$14,$15,$16,$17*3.6,$18*3.6,$19,$20,$21-273.15,$22,$23,$24,$25*3.6,$26*3.6,$27,$28-273.15,$29,$30,$31,$32*3.6,$33*3.6,$34,$35,$36,$37,$38-273.15,$39,$40,$41-273.15,$42,$43,$44*3.6,$45*3.6,$46,$47,$48,$49,$50,$51,$52,$53,$54,$55,$56,$57,$58,$59,$60,$61,$62,$63,$64,$65,$66,$67,$68,$69,$70/100,$71,$72-273.15,$73*3.6,$74*3.6,$75,$76,$77,$78,$79/100,(sqrt($44*$44+$45*$45))*3.6,(atan2($44,$45)*57.3+180),"SE"                   #North (Tramontana)
 	}
 	else if ((x > 155) && (x <= 205)) {
-		print $1,$2,$3*3.6,$4,$5-273.15,$6,$7,$8-273.15,$9,$10,$11-273.15,$12,$13,$14,$15,$16,$17-273.15,$18-273.15,$19,$20*3.6,$21*3.6,$22,$23*3600,$24*3600,$25,$26,$27,$28,$29,$30,$31/3600,$32,$33,$34,$35,$36,$37,$38,$39,$40,$41,$42/100,(sqrt($20*$20+$21*$21))*3.6,(atan2($20,$21)*57.3+180),"S"                   #South (Ostro)
+		print $1,$2,$3*3.6,$4,$5-273.15,$6,$7,$8,$9*3.6,$10*3.6,$11,$12,$13-273.15,$14,$15,$16,$17*3.6,$18*3.6,$19,$20,$21-273.15,$22,$23,$24,$25*3.6,$26*3.6,$27,$28-273.15,$29,$30,$31,$32*3.6,$33*3.6,$34,$35,$36,$37,$38-273.15,$39,$40,$41-273.15,$42,$43,$44*3.6,$45*3.6,$46,$47,$48,$49,$50,$51,$52,$53,$54,$55,$56,$57,$58,$59,$60,$61,$62,$63,$64,$65,$66,$67,$68,$69,$70/100,$71,$72-273.15,$73*3.6,$74*3.6,$75,$76,$77,$78,$79/100,(sqrt($44*$44+$45*$45))*3.6,(atan2($44,$45)*57.3+180),"S"                   #North (Tramontana)
 	}
 	else if ((x > 205) && (x <= 245)) {
-		print $1,$2,$3*3.6,$4,$5-273.15,$6,$7,$8-273.15,$9,$10,$11-273.15,$12,$13,$14,$15,$16,$17-273.15,$18-273.15,$19,$20*3.6,$21*3.6,$22,$23*3600,$24*3600,$25,$26,$27,$28,$29,$30,$31/3600,$32,$33,$34,$35,$36,$37,$38,$39,$40,$41,$42/100,(sqrt($20*$20+$21*$21))*3.6,(atan2($20,$21)*57.3+180),"SW"                  #South-West (Libeccio)
+		print $1,$2,$3*3.6,$4,$5-273.15,$6,$7,$8,$9*3.6,$10*3.6,$11,$12,$13-273.15,$14,$15,$16,$17*3.6,$18*3.6,$19,$20,$21-273.15,$22,$23,$24,$25*3.6,$26*3.6,$27,$28-273.15,$29,$30,$31,$32*3.6,$33*3.6,$34,$35,$36,$37,$38-273.15,$39,$40,$41-273.15,$42,$43,$44*3.6,$45*3.6,$46,$47,$48,$49,$50,$51,$52,$53,$54,$55,$56,$57,$58,$59,$60,$61,$62,$63,$64,$65,$66,$67,$68,$69,$70/100,$71,$72-273.15,$73*3.6,$74*3.6,$75,$76,$77,$78,$79/100,(sqrt($44*$44+$45*$45))*3.6,(atan2($44,$45)*57.3+180),"SW"                   #North (Tramontana)
 	}
 	else if ((x > 245) && (x <= 295)) {
-		print $1,$2,$3*3.6,$4,$5-273.15,$6,$7,$8-273.15,$9,$10,$11-273.15,$12,$13,$14,$15,$16,$17-273.15,$18-273.15,$19,$20*3.6,$21*3.6,$22,$23*3600,$24*3600,$25,$26,$27,$28,$29,$30,$31/3600,$32,$33,$34,$35,$36,$37,$38,$39,$40,$41,$42/100,(sqrt($20*$20+$21*$21))*3.6,(atan2($20,$21)*57.3+180),"W"                   #West (Ponente)
+		print $1,$2,$3*3.6,$4,$5-273.15,$6,$7,$8,$9*3.6,$10*3.6,$11,$12,$13-273.15,$14,$15,$16,$17*3.6,$18*3.6,$19,$20,$21-273.15,$22,$23,$24,$25*3.6,$26*3.6,$27,$28-273.15,$29,$30,$31,$32*3.6,$33*3.6,$34,$35,$36,$37,$38-273.15,$39,$40,$41-273.15,$42,$43,$44*3.6,$45*3.6,$46,$47,$48,$49,$50,$51,$52,$53,$54,$55,$56,$57,$58,$59,$60,$61,$62,$63,$64,$65,$66,$67,$68,$69,$70/100,$71,$72-273.15,$73*3.6,$74*3.6,$75,$76,$77,$78,$79/100,(sqrt($44*$44+$45*$45))*3.6,(atan2($44,$45)*57.3+180),"W"                   #North (Tramontana)
 	}
 	else if ((x > 295) && (x <= 335)) {
-		print $1,$2,$3*3.6,$4,$5-273.15,$6,$7,$8-273.15,$9,$10,$11-273.15,$12,$13,$14,$15,$16,$17-273.15,$18-273.15,$19,$20*3.6,$21*3.6,$22,$23*3600,$24*3600,$25,$26,$27,$28,$29,$30,$31/3600,$32,$33,$34,$35,$36,$37,$38,$39,$40,$41,$42/100,(sqrt($20*$20+$21*$21))*3.6,(atan2($20,$21)*57.3+180),"NW"                  #North-West (Maestrale)
+		print $1,$2,$3*3.6,$4,$5-273.15,$6,$7,$8,$9*3.6,$10*3.6,$11,$12,$13-273.15,$14,$15,$16,$17*3.6,$18*3.6,$19,$20,$21-273.15,$22,$23,$24,$25*3.6,$26*3.6,$27,$28-273.15,$29,$30,$31,$32*3.6,$33*3.6,$34,$35,$36,$37,$38-273.15,$39,$40,$41-273.15,$42,$43,$44*3.6,$45*3.6,$46,$47,$48,$49,$50,$51,$52,$53,$54,$55,$56,$57,$58,$59,$60,$61,$62,$63,$64,$65,$66,$67,$68,$69,$70/100,$71,$72-273.15,$73*3.6,$74*3.6,$75,$76,$77,$78,$79/100,(sqrt($44*$44+$45*$45))*3.6,(atan2($44,$45)*57.3+180),"NW"                   #North (Tramontana)
 	}
 }
-{if (NR>=3&&NR<=46) print windDirection((atan2($20,$21)*57.3+180)) }' p6d.csv > p6f.csv			# added function that transform direction from degree to a capital letter
+{if (NR>=3&&NR<=46) print windDirection((atan2($44,$45)*57.3+180)) }' p6d.csv > p6f.csv			# added function that transform direction from degree to a capital letter
 
 sed -i '/^$/d' p6f.csv						# delete void lines
 cat p6f.csv >> p6e.csv
@@ -131,7 +132,9 @@ sed -i -e 's/2_m_above_ground/2m/g;s/10_m_above_ground/10m/g;s/low_cloud_layer/l
 sed -i -e 's/middle_cloud_layer/middle_cloud/g;s/high_cloud_layer/high_cloud/g' p6e.csv
 sed -i -e 's/mean_sea_level/mean_sea/g;s/:00:00/:00/g' p6e.csv
 
-awk 'BEGIN{FS=OFS=","}{ print $1,$17,$18,$19,$20,$21,$43,$44,$45,$3,$35,$36,$37,$42,$23,$24,$25,$26,$30,$29,$28,$22,$27,$15,$32,$33,$38,$4,$7,$10,$14,$40,$41,$5,$8,$11,$6,$9,$12,$13,$2,$16,$39,$31,$10-$14,$7-$14,$4-$14,$7-$10,$4-$10,2.5-($17-$18),$34 }' p6e.csv > final.csv
+#awk 'BEGIN{FS=OFS=","}{ print $1,$17,$18,$19,$20,$21,$43,$44,$45,$3,$35,$36,$37,$42,$23,$24,$25,$26,$30,$29,$28,$22,$27,$15,$32,$33,$38,$4,$7,$10,$14,$40,$41,$5,$8,$11,$6,$9,$12,$13,$2,$16,$39,$31,$10-$14,$7-$14,$4-$14,$7-$10,$4-$10,2.5-($17-$18),$34 }' p6e.csv > final.csv
+#awk 'BEGIN{FS=OFS=","}{ print $1,$18,$22,$23,$24,$25,$54,$55,$56,$3,$42,$43,$44,$53,$27,$28,$29,$33,$30,$37,$36,$36,$38,$19,$40,$33,$45,$4,$8,$10,$15,$50,$51,$5,$8,$11,$7,$11,$12,$13,$3,$20,$46,$39,$10-$14,$8-$15,$4-$15,$8-$10,$4-$10,2.5-($18-$22),$41 }' p6e.csv > final.csv
+awk 'BEGIN{FS=OFS=","}{ print $1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19,$20,$21,$22,$23,$24,$25,$26,$27,$28,$29,$30,$31,$32,$33,$34,$35,$36,$37,$38,$39,$40,$41,$42,$43,$44,$45,$46,$47,$48,$49,$50,$51,$52,$53,$54,$55,$56,$57,$58,$59,$60,$61,$62,$63,$64,$65,$66,$67,$68,$69,$70,$71,$72,$73,$74,$75,$76,$77,$78,$79,$80,$81,$83,$20-$35,$12-$35,$4-$35,$12-$20,$4-$20,2.5-($41-$42) }' p6e.csv > final.csv
 
 cat final.csv | tr '"' ' ' > final.txt
 
@@ -173,5 +176,5 @@ gnuplot -e "run=$run" ./precTypes.pg > precTypes.png
 convert \( clouds.png precipitations.png cape-lftx.png -append \) \( temperatures.png hgt.png precTypes.png -append \) \( weather.png pressureWind.png  -append \) +append weatherForecastFinal.png
 
 # clean last created files
-# rm final2.csv final3.csv finalg.csv finalg.txt
+rm final2.csv finalg.csv finalg.txt
 rm anlFile
